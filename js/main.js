1146491f8a4043127d71c7a52cdc2781ad26a71b
dinesh00989 <dineshpalla0@gmail.com>
2026-01-20 15:48:32 +0530

    Updated multiple files

--- js/main.js
+++ js/main.js
@@ -48,3 +48,11 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 });
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+if (menuToggle && navMenu) {
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+}
